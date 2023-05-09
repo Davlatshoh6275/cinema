6dc5305cef4e0bc37d577c5326be5ca9f8e7c0ab
Pardaqulov Davlatshoh <davlatshoh6275@gmail.com>
2023-05-09 15:16:12 +0300

    now in cinema section

--- src/App.js
+++ src/App.js
@@ -10,130 +10,164 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import Typography from "@mui/material/Typography";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import SearchIcon from "@mui/icons-material/Search";
 
 export default function App() {
   return (
-    <Box
-      width={"100vw"}
-      height={"100vh"}
-      bgcolor={"#1E2538"}
-      boxSizing={"border-box"}
-      sx={{
-        margin: "0px",
-        padding: "0px",
-        backgroundImage: `url(${bgImg})`,
-        backgroundSize: "cover",
-      }}
-    >
+    <Box width={"100%"} height={"100%"} bgcolor={"#1E2538"}>
       <Box
+        width={"100%"}
+        height={"100vh"}
+        boxSizing={"border-box"}
         sx={{
-          width: "90%",
-          height: "100%",
-          margin: "0 auto",
+          margin: "0px",
+          padding: "0px",
+          backgroundImage: `url(${bgImg})`,
+          backgroundSize: "cover",
         }}
       >
         <Box
           sx={{
-            width: "100%",
-            height: "80px",
-            padding: "10px 0",
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
+            width: "90%",
+            height: "100%",
+            margin: "0 auto",
           }}
         >
-          <Box width={"5%"} height={"100%"}>
-            <img src={logo} alt="" width={"110px"} />
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                gap: "10px",
-              }}
-            >
-              <WhatsAppIcon sx={{ color: "#686868" }} fontSize={"small"} />
-              <InstagramIcon sx={{ color: "#fff" }} fontSize={"small"} />
-              <YouTubeIcon sx={{ color: "#686868" }} fontSize={"small"} />
-              <TwitterIcon sx={{ color: "#686868" }} fontSize={"small"} />
-            </Box>
-          </Box>
-
-          <List
+          <Box
             sx={{
-              width: "50%",
-              height: "100%",
-              padding: "0",
-              paddingBottom: "30px",
+              width: "100%",
+              height: "80px",
+              padding: "10px 0",
               display: "flex",
               justifyContent: "space-between",
               alignItems: "center",
             }}
           >
-            <ListItem sx={{ cursor: "pointer" }}>
-              <Typography variant="h6" sx={{ color: "#fff", fontSize: "16px" }}>
-                Афиша
-              </Typography>
-            </ListItem>
-            <ListItem sx={{ cursor: "pointer" }}>
-              <Typography variant="h6" sx={{ color: "#fff", fontSize: "16px" }}>
-                Медиа
-              </Typography>
-            </ListItem>
-            <ListItem sx={{ cursor: "pointer" }}>
-              <Typography variant="h6" sx={{ color: "#fff", fontSize: "16px" }}>
-                Фильмы
-              </Typography>
-            </ListItem>
-            <ListItem sx={{ cursor: "pointer" }}>
-              <Typography variant="h6" sx={{ color: "#fff", fontSize: "16px" }}>
-                Актёры
-              </Typography>
-            </ListItem>
-            <ListItem sx={{ cursor: "pointer" }}>
-              <Typography variant="h6" sx={{ color: "#fff", fontSize: "16px" }}>
-                Новости
-              </Typography>
-            </ListItem>
-            <ListItem sx={{ cursor: "pointer" }}>
-              <Typography variant="h6" sx={{ color: "#fff", fontSize: "16px" }}>
-                Подборки
-              </Typography>
-            </ListItem>
-            <ListItem sx={{ cursor: "pointer" }}>
-              <Typography variant="h6" sx={{ color: "#fff", fontSize: "16px" }}>
-                Категории
-              </Typography>
-            </ListItem>
-          </List>
-          <Box
-            sx={{
-              height: "100%",
-            }}
-          >
-            <Button size="small"  sx={{
-              width: "40px",
-              height: "40px",
-              bgcolor: "#fff",
-              marginRight: "12px",
-            }} >
-              <SearchIcon />
-              
-            </Button>
-            <Button size="large"  variant="contained" sx={{
-              background: "#3657CB",
-              boxShadow:" 0px 0px 15px rgba(72, 113, 255, 0.8)",
-              height: "40px"
-            }} >
-              <Typography variant="h6" sx={{
-                fontSize: "16px"
-              }} >Войти</Typography>
-            </Button>
+            <Box width={"5%"} height={"100%"}>
+              <img src={logo} alt="" width={"110px"} />
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  gap: "10px",
+                }}
+              >
+                <WhatsAppIcon sx={{ color: "#686868" }} fontSize={"small"} />
+                <InstagramIcon sx={{ color: "#fff" }} fontSize={"small"} />
+                <YouTubeIcon sx={{ color: "#686868" }} fontSize={"small"} />
+                <TwitterIcon sx={{ color: "#686868" }} fontSize={"small"} />
+              </Box>
+            </Box>
+
+            <List
+              sx={{
+                width: "50%",
+                height: "100%",
+                padding: "0",
+                paddingBottom: "30px",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <ListItem sx={{ cursor: "pointer" }}>
+                <Typography
+                  variant="h6"
+                  sx={{ color: "#fff", fontSize: "16px" }}
+                >
+                  Афиша
+                </Typography>
+              </ListItem>
+              <ListItem sx={{ cursor: "pointer" }}>
+                <Typography
+                  variant="h6"
+                  sx={{ color: "#fff", fontSize: "16px" }}
+                >
+                  Медиа
+                </Typography>
+              </ListItem>
+              <ListItem sx={{ cursor: "pointer" }}>
+                <Typography
+                  variant="h6"
+                  sx={{ color: "#fff", fontSize: "16px" }}
+                >
+                  Фильмы
+                </Typography>
+              </ListItem>
+              <ListItem sx={{ cursor: "pointer" }}>
+                <Typography
+                  variant="h6"
+                  sx={{ color: "#fff", fontSize: "16px" }}
+                >
+                  Актёры
+                </Typography>
+              </ListItem>
+              <ListItem sx={{ cursor: "pointer" }}>
+                <Typography
+                  variant="h6"
+                  sx={{ color: "#fff", fontSize: "16px" }}
+                >
+                  Новости
+                </Typography>
+              </ListItem>
+              <ListItem sx={{ cursor: "pointer" }}>
+                <Typography
+                  variant="h6"
+                  sx={{ color: "#fff", fontSize: "16px" }}
+                >
+                  Подборки
+                </Typography>
+              </ListItem>
+              <ListItem sx={{ cursor: "pointer" }}>
+                <Typography
+                  variant="h6"
+                  sx={{ color: "#fff", fontSize: "16px" }}
+                >
+                  Категории
+                </Typography>
+              </ListItem>
+            </List>
+            <Box
+              sx={{
+                height: "100%",
+              }}
+            >
+              <Button
+                size="small"
+                sx={{
+                  width: "40px",
+                  height: "40px",
+                  bgcolor: "#fff",
+                  marginRight: "12px",
+                }}
+              >
+                <SearchIcon />
+              </Button>
+              <Button
+                size="large"
+                variant="contained"
+                sx={{
+                  background: "#3657CB",
+                  boxShadow: " 0px 0px 15px rgba(72, 113, 255, 0.8)",
+                  height: "40px",
+                }}
+              >
+                <Typography
+                  variant="h6"
+                  sx={{
+                    fontSize: "16px",
+                  }}
+                >
+                  Войти
+                </Typography>
+              </Button>
+            </Box>
+          </Box>
+          <Box>
+            <HomePage />
           </Box>
         </Box>
-        <HomePage />
       </Box>
     </Box>
   );
--- src/components/homePage/NowInCinema.js
+++ src/components/homePage/NowInCinema.js
@@ -1,25 +1,118 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import { useState, useEffect } from 'react';
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectAllCinema, fetchCinemas } from '../../redux/card/NowInCinema';
+import { selectAllCinema, fetchCinemas } from "../../redux/card/NowInCinema";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import img from "../../img/homePage/img3.png";
 
 export default function NowInCinema() {
-
-  const dispatch = useDispatch()
-  const cinemas = useSelector(selectAllCinema)
+  const dispatch = useDispatch();
+  const cinemas = useSelector(selectAllCinema);
 
   useEffect(() => {
     dispatch(fetchCinemas());
   }, []);
-  
-  console.log(cinemas);
-
 
   return (
     <Box>
-
-        salom
+      <Box
+        sx={{
+          width: "100%",
+          height: "60px",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" sx={{ color: "#fff", fontWeight: "900" }}>
+          Сейчас в кино
+        </Typography>
+        <Box
+          style={{ width: "50px", height: "1px", backgroundColor: "#fff" }}
+        ></Box>
+        <List
+          sx={{
+            width: "60%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <ListItem sx={{ cursor: "pointer" }}>
+            <Typography
+              variant="h6"
+              sx={{ color: "#fff", fontSize: "16px", fontWeight: "400" }}
+            >
+              Все
+            </Typography>
+          </ListItem>
+          <ListItem sx={{ cursor: "pointer" }}>
+            <Typography
+              variant="h6"
+              sx={{ color: "#6F717B", fontSize: "16px", fontWeight: "400" }}
+            >
+              Боевики
+            </Typography>
+          </ListItem>
+          <ListItem sx={{ cursor: "pointer" }}>
+            <Typography
+              variant="h6"
+              sx={{ color: "#6F717B", fontSize: "16px", fontWeight: "400" }}
+            >
+              Приключения
+            </Typography>
+          </ListItem>
+          <ListItem sx={{ cursor: "pointer" }}>
+            <Typography
+              variant="h6"
+              sx={{ color: "#6F717B", fontSize: "16px", fontWeight: "400" }}
+            >
+              Комедии
+            </Typography>
+          </ListItem>
+          <ListItem sx={{ cursor: "pointer" }}>
+            <Typography
+              variant="h6"
+              sx={{ color: "#6F717B", fontSize: "16px", fontWeight: "400" }}
+            >
+              Фантастика
+            </Typography>
+          </ListItem>
+          <ListItem sx={{ cursor: "pointer" }}>
+            <Typography
+              variant="h6"
+              sx={{ color: "#6F717B", fontSize: "16px", fontWeight: "400" }}
+            >
+              Триллеры
+            </Typography>
+          </ListItem>
+          <ListItem sx={{ cursor: "pointer" }}>
+            <Typography
+              variant="h6"
+              sx={{ color: "#6F717B", fontSize: "16px", fontWeight: "400" }}
+            >
+              Драма
+            </Typography>
+          </ListItem>
+        </List>
+      </Box>
+      <Box
+        sx={{
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        {cinemas.map((item, index) => (
+          <>
+            <img src={item.img} alt="poter" />
+            <Typography variant="h4">{item.name}</Typography>
+            {console.log(item)}
+          </>
+        ))}
+      </Box>
     </Box>
-  )
+  );
 }
